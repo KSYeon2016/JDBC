2d44f1ad5bc41d3629fbb62c41bb94786ce85349
S401-08 <S401-08@S401-08>
2016-07-28 15:47:38 +0900

    BookDao

--- src/kr/ac/sungkyul/bookmaill/app/BookMall.java
+++ src/kr/ac/sungkyul/bookmaill/app/BookMall.java
@@ -3,13 +3,18 @@ package kr.ac.sungkyul.bookmaill.app;
 import java.util.List;
 
 import kr.ac.sungkyul.bookmaill.dao.AuthorDao;
+import kr.ac.sungkyul.bookmaill.dao.BookDao;
 import kr.ac.sungkyul.bookmall.vo.AuthorVo;
+import kr.ac.sungkyul.bookmall.vo.BookVo;
 
 public class BookMall {
 
 	public static void main(String[] args) {
-		testAuthorDaoInsert();
+		//testAuthorDaoInsert();
 		testAuthorDaoGetList();
+		
+		//testBookDaoInsert();
+		testBookDaoGetList();
 	}
 
 	public static void testAuthorDaoGetList(){
@@ -27,6 +32,25 @@ public class BookMall {
 		vo.setDescription("");
 		
 		AuthorDao dao = new AuthorDao();
+		dao.insert(vo);
+	}
+	
+	public static void testBookDaoGetList(){
+		BookDao dao = new BookDao();
+		List<BookVo> list = dao.getList();
+		
+		for(BookVo vo : list){
+			System.out.println(vo);
+		}
+	}
+	
+	public static void testBookDaoInsert(){
+		BookVo vo = new BookVo();
+		vo.setTitle("토지");
+		vo.setRate(4);
+		vo.setAuthorNo(1L);
+		
+		BookDao dao = new BookDao();
 		dao.insert(vo);
 	}
 }
